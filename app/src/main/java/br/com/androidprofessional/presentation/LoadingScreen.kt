b4ffcecf0239d18ca3604ad725f6763b2ca4db5f
cleberschuster <cleber.schuster.pro@gmail.com>
2024-05-14 09:33:38 -0300

    refactor code

--- app/src/main/java/br/com/androidprofessional/presentation/LoadingScreen.kt
+++ app/src/main/java/br/com/androidprofessional/presentation/LoadingScreen.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
@@ -24,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -40,6 +42,30 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun LoadingView(context: Context) {
     val viewModel: ObserveStateViewModel = koinViewModel()
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(top = 16.dp)
+    ) {
+        Card(
+            modifier = Modifier
+                .size((LocalConfiguration.current.screenWidthDp.dp)/0),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(8.dp),
+            shape = RoundedCornerShape(10),
+        ) {
+
+            CarsContent(context, viewModel)
+        }
+    }
+}
+
+@Composable
+fun CarsContent(context: Context, viewModel: ObserveStateViewModel) {
+
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val lifecycleOwner = LocalLifecycleOwner.current
     val disposable: () -> Unit = {}
@@ -50,117 +76,87 @@ fun LoadingView(context: Context) {
         }
     }
 
-//    Column {
-//        Text("Counter value: ${uiState.data}")
-//        Button(onClick = { viewModel.getNewComment(2) }) {
-//            Text("Increment")
-//        }
-////        Button(onClick = { viewModel.getNewComment(binding.searchEditText.text.toString().toInt()) }) {
-////            Text("Increment")
-////        }
-//    }
+    //Inicio Faz a Busca quando clica no botao
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
 
-    when (uiState.status) {
+        val textState = remember { mutableStateOf("") }
+        TextField(
+            placeholder = { Text("Pesquisar") },
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+        IconButton(onClick = {
+            viewModel.getNewComment(textState.value.toInt())
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_warning),
+                contentDescription = null,
+            )
+        }
+        Text("INFO: ${textState.value}")
+    }//Fim Faz a Busca quando clica no botao
 
-        Status.LOADING -> Text("Carregando...")
-
-        Status.SUCCESS -> {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 16.dp)
-            ) {
-                Card(
-//                modifier = Modifier
-//                    .size((LocalConfiguration.current.screenWidthDp.dp)/0),
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color.White
-                    ),
-                    elevation = CardDefaults.cardElevation(8.dp),
-                    shape = RoundedCornerShape(10),
-                ) {
-
-                    //Inicio Faz a Busca quando clica no botao
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-
-                        val textState = remember { mutableStateOf("") }
-                        TextField(
-                            placeholder = { Text("Pesquisar") },
-                            value = textState.value,
-                            onValueChange = { textState.value = it },
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Number
-                            )
-                        )
-                        IconButton(onClick = {
-                            viewModel.getNewComment(textState.value.toInt())
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_warning),
-                                contentDescription = null,
-                            )
-                        }
-                        Text("INFO: ${textState.value}")
-                    }//Fim Faz a Busca quando clica no botao
-
-                    //Inicio Faz uma nova Busca a cada caractere digitado
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            var search by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-                                mutableStateOf(TextFieldValue())
-                            }
-
-                        SearchTextField(
-                            value = search, onValueChange = {
-                                search = it
-                                viewModel.getNewComment(it.text.toInt())
-                            }, hint = stringResource(R.string.search),
-                            color = MaterialTheme.colorScheme.background
-                        )
-                        IconButton(onClick = {
-                            viewModel.getNewComment(search.text.toInt())
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_warning),
-                                contentDescription = null,
-                            )
-                        }
-                    }                     //Fim Faz uma nova Busca a cada caractere digitado
-
-                    Text(
-                        modifier = Modifier.padding(vertical = 16.dp),
-                        text = "COMENTARIO: ${uiState.data?.comment}",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 16.dp),
-                        text = "EMAIL: ${uiState.data?.email}",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 16.dp),
-                        text = "NOME: ${uiState.data?.name}",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 16.dp),
-                        text = "ID: ${uiState.data?.id}",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                    Text(
-                        modifier = Modifier.padding(vertical = 16.dp),
-                        text = "POST ID: ${uiState.data?.postId}",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
-
-            }
+    //Inicio Faz uma nova Busca a cada caractere digitado
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        var search by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+            mutableStateOf(TextFieldValue())
         }
 
+        SearchTextField(
+            value = search, onValueChange = {
+                search = it
+                viewModel.getNewComment(it.text.toInt())
+            }, hint = stringResource(R.string.search),
+            color = MaterialTheme.colorScheme.background
+        )
+        IconButton(onClick = {
+            viewModel.getNewComment(search.text.toInt())
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_warning),
+                contentDescription = null,
+            )
+        }
+    }                     //Fim Faz uma nova Busca a cada caractere digitado
+
+    when (uiState.status) {
+        Status.SUCCESS -> {
+
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "COMENTARIO: ${uiState.data?.comment}",
+                style = TextStyle(fontSize = 16.sp)
+            )
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "EMAIL: ${uiState.data?.email}",
+                style = TextStyle(fontSize = 16.sp)
+            )
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "NOME: ${uiState.data?.name}",
+                style = TextStyle(fontSize = 16.sp)
+            )
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "ID: ${uiState.data?.id}",
+                style = TextStyle(fontSize = 16.sp)
+            )
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "POST ID: ${uiState.data?.postId}",
+                style = TextStyle(fontSize = 16.sp)
+            )
+        }
         Status.ERROR -> Toast.makeText(context, "Ocorreu um erro ${uiState.message}", Toast.LENGTH_LONG).show()
+        Status.LOADING -> Text("Carregando...")
     }
 }
 
