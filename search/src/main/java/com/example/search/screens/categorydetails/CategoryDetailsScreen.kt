3d6443ce12986acb1fc289a36bde3d557519aa5f
cleberschuster <cleber.schuster.pro@gmail.com>
2024-06-18 12:18:55 -0300

    replace ArrowBack

--- search/src/main/java/com/example/search/screens/categorydetails/CategoryDetailsScreen.kt
+++ search/src/main/java/com/example/search/screens/categorydetails/CategoryDetailsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -35,7 +36,7 @@ fun CategoryDetailsScreen(category: MealCategory, navigateToHomeScreen: () -> Un
                 navigationIcon = {
                     IconButton(onClick = navigateToHomeScreen) {
                         Icon(
-                            imageVector = Icons.Filled.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back to Home Screen"
                         )
                     }
