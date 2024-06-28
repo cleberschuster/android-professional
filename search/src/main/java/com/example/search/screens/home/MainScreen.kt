1c361274064f6436db7884e5f6505e7816bea8bf
cleberschuster <cleber.schuster.pro@gmail.com>
2024-06-28 09:51:20 -0300

    add SoftwareKeyboardController

--- search/src/main/java/com/example/search/screens/home/MainScreen.kt
+++ search/src/main/java/com/example/search/screens/home/MainScreen.kt
@@ -2,11 +2,19 @@ package com.example.search.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,7 +22,9 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -30,36 +40,48 @@ fun MainScreen() {
 
     //Collecting states from ViewModel
     val searchText by viewModel.searchText.collectAsStateWithLifecycle()
-//    val isSearching by viewModel.isSearching.collectAsState()
     val countriesList by viewModel.countriesList.collectAsStateWithLifecycle()
+//    val isSearching by viewModel.isSearching.collectAsStateWithLifecycle()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Scaffold(
         topBar = {
             SearchBar(
                 query = searchText,//text showed on SearchBar
                 onQueryChange = viewModel::onSearchTextChange, //update the value of searchText
-                onSearch = {}, //the callback to be invoked when the input service triggers the ImeAction.Search action
+                onSearch = { keyboardController?.hide() }, //the callback to be invoked when the input service triggers the ImeAction.Search action
                 active = true, //whether the user is searching or not
                 onActiveChange = {}, //the callback to be invoked when this search bar's active state is changed
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                LazyColumn {
-                    items(countriesList) { country ->
-                        Image(
-                            painter = rememberAsyncImagePainter(country.strCategoryThumb),
-                            contentDescription = country.strCategoryDescription,
-                            modifier = Modifier.aspectRatio(1f)
-                        )
-                        Text(
-                            text = "${country.idCategory} - ${country.strCategory}",
-                            style = MaterialTheme.typography.headlineMedium,
-                            modifier = Modifier.padding(top = 4.dp)
-                        )
+                Spacer(modifier = Modifier.height(16.dp))
+//                if(isSearching) {
+//                    Box(modifier = Modifier.fillMaxSize()) {
+//                        CircularProgressIndicator(
+//                            modifier = Modifier.align(Alignment.Center)
+//                        )
+//                    }
+//                } else {
+//                    LazyColumn {
+                    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+                        items(countriesList) { country ->
+                            Image(
+                                painter = rememberAsyncImagePainter(country.strCategoryThumb),
+                                contentDescription = country.strCategoryDescription,
+                                modifier = Modifier.aspectRatio(1f)
+                            )
+                            Text(
+                                text = "${country.idCategory} - ${country.strCategory}",
+                                style = MaterialTheme.typography.headlineMedium,
+                                modifier = Modifier.padding(top = 4.dp)
+                            )
+                        }
                     }
                 }
-            }
+//            }
         }
     ) {
 
--- search/src/main/java/com/example/search/screens/home/SearchViewModel.kt
+++ search/src/main/java/com/example/search/screens/home/SearchViewModel.kt
@@ -5,11 +5,16 @@ import androidx.lifecycle.viewModelScope
 import com.example.search.api.recipeService
 import com.example.search.dataclasses.HomeScreenState
 import com.example.search.dataclasses.MealCategory
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SearchViewModel : ViewModel() {
@@ -17,6 +22,9 @@ class SearchViewModel : ViewModel() {
     private val _homeScreenState = MutableStateFlow(HomeScreenState())
     private val _searchText = MutableStateFlow("")
     val searchText = _searchText.asStateFlow()
+
+//    private val _isSearching = MutableStateFlow(false)
+//    val isSearching = _isSearching.asStateFlow()
 
     //third state the list to be filtered
     private val _countriesList = MutableStateFlow(_homeScreenState.value.categories)
@@ -37,6 +45,29 @@ class SearchViewModel : ViewModel() {
             started = SharingStarted.WhileSubscribed(5000),//it will allow the StateFlow survive 5 seconds before it been canceled
             initialValue = _countriesList.value
         )
+
+
+    // Outra forma de executar a busca, se necessario com delay
+//    @OptIn(FlowPreview::class)
+//    val countriesList = searchText
+//        .debounce(1000L)
+//        .onEach { _isSearching.update { true } }
+//        .combine(_countriesList) { text, persons ->
+//            if(text.isBlank()) {
+//                persons
+//            } else {
+//                delay(2000L)
+//                persons.filter {
+//                    doesMatchSearchQuery(it, text)
+//                }
+//            }
+//        }
+//        .onEach { _isSearching.update { false } }
+//        .stateIn(
+//            viewModelScope,
+//            SharingStarted.WhileSubscribed(5000),
+//            _countriesList.value
+//        )
 
     fun onSearchTextChange(text: String) {
         _searchText.value = text
